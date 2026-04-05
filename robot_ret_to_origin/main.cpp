b490e696dbf82594fd2773d16cdd2eeffa4feb42
Oleh Novosad <olehnovosad2013@gmail.com>
2026-04-05 21:55:38 +0100

    Robot Return to Origin

--- robot_ret_to_origin/main.cpp
+++ robot_ret_to_origin/main.cpp
@@ -1,23 +1,30 @@
 #include <array>
-#include <ios>
 #include <iostream>
-#include <map>
 #include <string>
-#include <unordered_map>
 
 class Solution {
 public:
     bool judgeCircle(std::string moves) {
-        std::unordered_map<char, std::array<int, 2>> directions = {
-            {'L', {-1, 0}}, {'R', {1, 0}}, {'U', {0, 1}}, {'D', {0, -1}}};
         std::array<int, 2> path = {0, 0};
 
         for (auto const& move : moves) {
-            path[0] = path[0] + directions[move][0];
-            path[1] = path[1] + directions[move][1];
+            switch (move) {
+                case 'R':
+                    path[0]++;
+                    break;
+                case 'L':
+                    path[0]--;
+                    break;
+                case 'U':
+                    path[1]++;
+                    break;
+                case 'D':
+                    path[1]--;
+                    break;
+                default:
+                    break;
+            }
         }
-
-        std::cout << path[0] << " | " << path[1] << std::endl;
 
         if (path[0] != 0 || path[1] != 0) {
             return false;
